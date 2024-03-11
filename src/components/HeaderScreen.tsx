e7aaa2775b828fafb3491d83fce5645c509a3f2c
David Messi <dcrmesssi@gmail.com>
2024-03-10 21:02:56 -0500

    Ajustes Finales

--- src/components/HeaderScreen.tsx
+++ src/components/HeaderScreen.tsx
@@ -1,10 +1,10 @@
 
-import React, { useState } from 'react';
+import { useState } from 'react';
 
 import { KeyboardArrowDown } from '@mui/icons-material';
 import { AppBar, Box, Button, Menu, MenuItem, styled } from '@mui/material';
 
-// const navItems = ['Home', 'Pages', 'About', 'Contact'];
+const navItems = ['Home', 'Pages', 'About', 'Contact'];
 
 
 
@@ -13,46 +13,11 @@ const CustomButton = styled(Button)(({ theme }:any) => ({
     fontSize: 16,
     fontWeight: 'medium', 
     '&:hover': {
-      color: theme.palette.primary.main, 
-      backgroundColor: 'gray', 
+    color: theme.palette.primary.main, 
+    backgroundColor: 'gray', 
     },
-  }));
+}));
 
-
-
-  const navItems = [
-    {
-      label: 'Home',
-    },
-    {
-      label: 'About',
-      subItems: [
-        {
-          label: 'Our Mission',
-        },
-        {
-          label: 'Our Team',
-        },
-      ],
-    },
-    {
-      label: 'Products',
-      subItems: [
-        {
-          label: 'Product A',
-        },
-        {
-          label: 'Product B',
-        },
-        {
-          label: 'Product C',
-        },
-      ],
-    },
-    {
-      label: 'Contact',
-    },
-  ];
 
 
 
@@ -60,71 +25,49 @@ export const HeaderScreen = () => {
 
 
     const [anchorEl, setAnchorEl] = useState<any>(null);
+    const [valor, setValor] = useState<any>(null);
 
 
-    const handleOpenMenu = (event:string) => {
-        setAnchorEl(event);
+
+    const handleClick = (event:any, valor:string) => {
+        setAnchorEl(event.currentTarget);
+        setValor(valor);
     };
-  
-    const handleCloseMenu = () => {
+
+
+    
+    const handleClose = () => {
         setAnchorEl(null);
+        setValor(null);
     };
 
 
 
     return (
-        // <AppBar component="nav" sx={{height:100, width:'100%', display:'flex', justifyContent:'space-evenly', alignItems:'center'}}>
-        //     <Box sx={{display:'flex',  width:'100%', justifyContent:'space-evenly', alignItems:'center'}}>
-        //         {navItems.map((item) => (
-        //         <CustomButton key={item} sx={{ color: '#fff' }}>
-        //             {item}
-        //         </CustomButton>
-        //         ))}
-        //     </Box>
-        // </AppBar>
 
-        <AppBar component="nav" sx={{ height: 100, width: '100%', display: 'flex', justifyContent: 'space-evenly', alignItems: 'center', position: 'fixed' }}>
+
+        <AppBar component="nav" sx={{ height: 100, width: '100%', display: 'flex', justifyContent: 'space-evenly', alignItems: 'center' }}>
             <Box sx={{ display: 'flex', width: '100%', justifyContent: 'space-evenly', alignItems: 'center' }}>
-                {navItems.map((item) => {
-                if (item.subItems) {
-                    return (
-                    <React.Fragment key={item.label}>
-                        <CustomButton
-                            endIcon={<KeyboardArrowDown />}
-                            aria-controls={item.label}
-                            aria-haspopup="true"
-                            onClick={ () => handleOpenMenu(item.label)}
-                            sx={{ mr: 2 }}
-                        >
-                        {item.label}
-                        </CustomButton>
-                        <Menu
-                            sx={{ mt:'0px', width:'300px' }}
-                            anchorEl={anchorEl}
-                            open={Boolean(anchorEl === item.label)}
-                            onClose={handleCloseMenu}
-                            MenuListProps={{
-                                sx: {backgroundColor: 'red' },
-                            }}
-                            anchorOrigin={{ vertical: 'bottom', horizontal: 'right' }}
-                            transformOrigin={{ vertical: 'top', horizontal: 'right' }} 
-                        >
-                        {item.subItems.map((subItem) => (
-                            <MenuItem key={subItem.label} onClick={handleCloseMenu}>
-                            {subItem.label}
-                            </MenuItem>
-                        ))}
-                        </Menu>
-                    </React.Fragment>
-                    );
-                } else {
-                    return (
-                    <CustomButton key={item.label} sx={{ mr: 2 }}>
-                        {item.label}
+                {navItems.map((item:any) => (
+                <div key={item}>
+                    <CustomButton sx={{ color: '#fff' }} endIcon={ (item === 'Pages') ? <KeyboardArrowDown /> : ''}
+                        onClick={ (evento) => handleClick(evento, item) }
+                    >
+                        {item}
                     </CustomButton>
-                    );
-                }
-                })}
+                    <Menu
+                        anchorEl={anchorEl}
+                        open={Boolean(valor === 'Pages')}
+                        onClose={ handleClose }
+                        anchorOrigin={{ vertical: 'bottom', horizontal: 'center' }}
+                        transformOrigin={{ vertical: 'top', horizontal: 'center' }}
+                    >
+                        <MenuItem onClick={ handleClose }>Subitem 1</MenuItem>
+                        <MenuItem onClick={ handleClose }>Subitem 2</MenuItem>
+                        <MenuItem onClick={ handleClose }>Subitem 3</MenuItem>
+                    </Menu>
+                </div>
+                ))}
             </Box>
         </AppBar>
     )
